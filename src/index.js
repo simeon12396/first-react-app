1bc5834c8b0eea13c0927a3567e4d114331a4c6b
Monkata96 <sieroteev@gmail.com>
2019-09-25 09:07:28 +0300

    Added a stater pack by React.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// App component - root component
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = () => {
+    return(<h1> Hello from React.js - Simo</h1>)
+};
+
+ReactDOM.render( <App/> , document.getElementById('root'));
