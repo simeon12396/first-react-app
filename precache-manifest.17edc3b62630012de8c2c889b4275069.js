cad497b3169c57b324eb5a84ab960834050a440b
Monkata96 <sieroteev@gmail.com>
2020-01-13 14:46:43 +0200

    Updates

---
+++ precache-manifest.17edc3b62630012de8c2c889b4275069.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cab8e246f549c8c1513478dee2d2edb4",
+    "url": "/simeon12396/first-react-app.git/index.html"
+  },
+  {
+    "revision": "eaff9ab42a9dae70057e",
+    "url": "/simeon12396/first-react-app.git/static/css/2.6f35021b.chunk.css"
+  },
+  {
+    "revision": "eecb731412ca2840f677",
+    "url": "/simeon12396/first-react-app.git/static/css/main.392f9686.chunk.css"
+  },
+  {
+    "revision": "eaff9ab42a9dae70057e",
+    "url": "/simeon12396/first-react-app.git/static/js/2.f09e0e2b.chunk.js"
+  },
+  {
+    "revision": "eecb731412ca2840f677",
+    "url": "/simeon12396/first-react-app.git/static/js/main.5afe2ad6.chunk.js"
+  },
+  {
+    "revision": "875481b78c9b8b7e7814",
+    "url": "/simeon12396/first-react-app.git/static/js/runtime-main.46c92df9.js"
+  },
+  {
+    "revision": "ff63ad61d10509fabeb13ec7e810e233",
+    "url": "/simeon12396/first-react-app.git/static/media/login-bck.ff63ad61.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/simeon12396/first-react-app.git/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/simeon12396/first-react-app.git/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/simeon12396/first-react-app.git/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/simeon12396/first-react-app.git/static/media/slick.f97e3bbf.svg"
+  }
+]);
